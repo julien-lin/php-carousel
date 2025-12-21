affcfa24889b8a82950f2d10f49e29b42d4b1d3d
julien <julien@MacBook-Pro-de-julien.local>
2025-12-21 11:22:31 +0100

    fix: Correct CSS minifier regex for zero units removal

--- src/Carousel/Helper/CssMinifier.php
+++ src/Carousel/Helper/CssMinifier.php
@@ -19,7 +19,7 @@ class CssMinifier
     {
         // Remove comments (including those with special characters)
         $css = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css);
-        
+
         // Preserve strings (quoted values) before processing
         $strings = [];
         $stringIndex = 0;
@@ -33,10 +33,10 @@ class CssMinifier
             },
             $css
         );
-        
+
         // Remove whitespace (but preserve in calc(), attr(), etc.)
         $css = preg_replace('/\s+/', ' ', $css);
-        
+
         // Remove spaces around operators (but preserve in calc())
         $css = preg_replace_callback(
             '/calc\([^)]+\)/',
@@ -45,50 +45,49 @@ class CssMinifier
             },
             $css
         );
-        
+
         // Remove spaces around operators (but preserve colons)
         $css = preg_replace('/\s*([{};,])\s*/', '$1', $css);
-        
+
         // Remove spaces around colons (but preserve in URLs and calc)
         $css = preg_replace('/\s*:\s*(?![^()]*\))/', ':', $css);
-        
+
         // Remove trailing semicolons before closing braces
         $css = preg_replace('/;}/', '}', $css);
-        
+
         // Remove spaces before opening braces
         $css = preg_replace('/\s*{/', '{', $css);
-        
+
         // Remove spaces after opening braces
         $css = preg_replace('/{\s+/', '{', $css);
-        
+
         // Remove spaces before closing braces
         $css = preg_replace('/\s*}/', '}', $css);
-        
+
         // Remove spaces after closing braces
         $css = preg_replace('/}\s+/', '}', $css);
-        
+
         // Remove spaces after semicolons
         $css = preg_replace('/;\s+/', ';', $css);
-        
+
         // Remove spaces after commas (but preserve in rgba, etc.)
         $css = preg_replace('/,\s+/', ',', $css);
-        
+
         // Remove leading zeros in decimal numbers (0.5 -> .5, but not 0px)
         $css = preg_replace('/([^0-9])0+\.([0-9]+)/', '$1.$2', $css);
-        
+
         // Remove units from zero values (0px -> 0, 0em -> 0) but preserve in calc()
         // Match: space or colon before 0, unit, then space or semicolon or closing brace
-        $css = preg_replace('/([: ])0(px|em|rem|pt|pc|in|cm|mm|ex|ch|vw|vh|vmin|vmax|%|deg|rad|grad|ms|s|Hz|kHz)([; }])/i', '$10$3', $css);
-        
+        $css = preg_replace('/([: ])0(px|em|rem|pt|pc|in|cm|mm|ex|ch|vw|vh|vmin|vmax|%|deg|rad|grad|ms|s|Hz|kHz)([; }])/i', '${1}0$3', $css);
+
         // Shorten color values (#ffffff -> #fff, #000000 -> #000)
         $css = preg_replace('/#([0-9a-f])\1([0-9a-f])\2([0-9a-f])\3/i', '#$1$2$3', $css);
-        
+
         // Restore strings
         foreach ($strings as $index => $string) {
             $css = str_replace('___STRING_' . $index . '___', $string, $css);
         }
-        
+
         return trim($css);
     }
 }
-
